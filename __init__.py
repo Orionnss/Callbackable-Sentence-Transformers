7d8da4e5df0c106abc870b24d871c70478a84276
Cyrille Siouffi-Degraeve <cyrille.siouffi@protonmail.com>
2024-04-17 14:45:48 +0200

    fix: added torch.nn dependency

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from .src.callbackable_sentence_transformers import callbackable_sentence_transformers
+from .src.callbackable_sentence_transformers.callbackable_sentence_transformers import SentenceTransformer as CallbackableSentenceTransformer
--- src/sentence_transformers/__init__.py
+++
@@ -1,5 +0,0 @@
-# __init__.py
-
-from importlib import resources
-
-__version__ = "1.0.0"
